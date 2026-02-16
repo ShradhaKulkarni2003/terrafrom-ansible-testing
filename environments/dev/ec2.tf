f68c365b3fb2b27f9e50136654f40dd6f9117721
ShradhaKulkarni2003 <shradha.kulkarni@neutrinotechlabs.com>
2026-02-16 16:25:30 +0530

    Initial commit: Terraform infrastructure code

---
+++ environments/dev/ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_key_pair" "devops" {
+  key_name   = "devops-key"
+#   public_key = file("~/.ssh/devops-key.pub")
+   public_key = file("/home/shradha/.ssh/devops-key.pub")
+}
+
+resource "aws_instance" "dev" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.devops.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "dev-ec2"
+  }
+}
+
